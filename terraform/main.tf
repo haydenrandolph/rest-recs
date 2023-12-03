ad23347c367293987e813a5c0466078126807bf5
Hayden Randolph <hayden.randolph@bp.com>
2023-12-02 22:47:39 -0500

    removing log sink

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "google_cloud_run_service" "api_service" {
   }
 }
 
-# allow unauthenticated invocations
+# Allow unauthenticated invocations
 resource "google_cloud_run_service_iam_policy" "api_service_policy" {
   location    = google_cloud_run_service.api_service.location
   project     = var.project_id
@@ -33,24 +33,8 @@ resource "google_cloud_run_service_iam_policy" "api_service_policy" {
   EOF
 }
 
-//bucket to capture logs
+# Bucket to capture logs
 resource "google_storage_bucket" "log_bucket" {
   name     = "rest-rec-logs-${random_id.bucket_suffix.hex}"
   location = var.region
-}
-
-resource "google_logging_project_sink" "log_sink" {
-  name                   = "rest-rec-log-sink"
-  destination            = "storage.googleapis.com/${google_storage_bucket.log_bucket.name}"
-  filter                 = "resource.type = \"cloud_function\" AND resource.labels.function_name = \"${google_cloudfunctions_function.docker_function.name}\""
-  unique_writer_identity = true
-}
-
-resource "google_storage_bucket_iam_binding" "log_bucket_writer" {
-  bucket = google_storage_bucket.log_bucket.name
-  role   = "roles/storage.objectCreator"
-
-  members = [
-    google_logging_project_sink.log_sink.writer_identity,
-  ]
 }
